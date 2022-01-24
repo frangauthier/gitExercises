5ee48505a2c6366bb464b9a3126e26576511ac62
Francis Gauthier <fgauthier@crc-lennox.qc.ca>
2022-01-24 08:13:43 -0500

    Updated shoppin list and added get /fruits route

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', function(req, res) {
+    res.send('Hello World')
+})
+
+app.get('/fruits', function(req, res) {
+    res.send({
+        fruits: ['apple', 'banana', 'mango'],
+        edible: true
+    })
 })
 
 app.listen(3000)
