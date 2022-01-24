8179d0084a1787d55bc92672ccabd3350a60bd31
Francis Gauthier <fgauthier@crc-lennox.qc.ca>
2022-01-24 08:16:36 -0500

    Updated shopping list and added post /fruits route

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', function(req, res) {
+    res.send('Hello World')
+})
+
+app.post('/fruits', function(req, res) {
+    // TODO add fruit to DB
+    res.status(200)
+    res.send('Ok')
 })
 
 app.listen(3000)
